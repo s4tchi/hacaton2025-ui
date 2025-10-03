f3c6c80758a6a3a9fcc7e0bda59150af861ff549
satchi <andrei.moskalenko@1win.pro>
2025-10-04 01:06:03 +0300

    store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { useGeometryPoints, useSocket } from "./hook"
 
 const App = () => {
+  const { setCurrentPosition } = useGeometryPoints();
+  const { isConnected } = useSocket({ handleSyncObjectPosition: (value: string) => {
+    setCurrentPosition(JSON.parse(value));
+  }});
 
-  return (
-    <div>
-      hello world
-    </div>
-  )
+  return isConnected ? <p>Connected</p> : <p>Disconnected</p>
 }
 
 export default App
---
+++ src/hook/useGeometryPoints.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand';
+import { IPoint, ISensor } from '../interface';
+
+interface IGeometryPoints {
+    currentPosition: IPoint;
+    positionHistory: Array<IPoint>;
+    beacons: Array<ISensor>;
+
+    setCurrentPosition: (point: IPoint) => void;
+    removeHistory: () => void;
+}
+
+export const useGeometryPoints = create<IGeometryPoints>((set) => ({
+    currentPosition: { x: 0, y: 0 },
+    positionHistory: [],
+    beacons: [],
+
+    setCurrentPosition: (point: IPoint) => set((state: IGeometryPoints) => ({ currentPosition: point, positionHistory: [...state.positionHistory, point] })),
+    removeHistory: () => set(() => ({ positionHistory: [] })),
+})); 
