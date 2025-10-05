92fef3894f069f759d5397ed1453ed8a4f7cc048
maria <soldatenko@gmail.com>
2025-10-05 10:54:45 +0300

    fixed dialog

--- src/api/updateSensors.ts
+++ src/api/updateSensors.ts
@@ -3,7 +3,7 @@ import { API_URL } from "../utils/api_constants"
 import { ISensor } from "../interface";
 
 export const updateSensors = async (id: number, body: ISensor) => {
-  const response = await axios.put(`${API_URL}/anchors/${id}`, {
+  const response = await axios.patch(`${API_URL}/anchors/${id}`, {
     name: body.name,
     x_coord: body.x,
     y_coord: body.y,
